e3fadf8cf95e489479da194bf0e554402fdf4a5a
CozyWool <vsergeev201530@gmail.com>
2025-06-19 21:44:21 +0500

    task-15: сделал обработку http ошибок

---
+++ WebChatApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebChatApplication.Models;
+
+public class ErrorViewModel
+{
+    public string Title { get; set; }
+    public string Message { get; set; }
+    public int StatusCode { get; set; }
+}
